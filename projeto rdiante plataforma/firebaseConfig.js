16ab393e4c0170271a5ef8f9afef9e43dd324821
JoandsonSilva <91487025+JoandsonSilva@users.noreply.github.com>
2025-04-26 14:00:07 -0300

    atualizado

--- projeto rdiante plataforma/firebaseConfig.js
+++ projeto rdiante plataforma/firebaseConfig.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,3 +20,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Exporta os serviços que você usará (Auth e Firestore)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
